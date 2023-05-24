e65dd6cfe45e4c303e79ffc99f5f4f756e01acc6
SigneWehlast <119939548+SigneWehlast@users.noreply.github.com>
2023-05-24 13:16:25 +0200

    js til kategorisiden

---
+++ scripts/category.js
@@ -0,0 +1,19 @@
+function omkostningerScroll() {
+document.getElementById("omkostninger-id").scrollIntoView();
+}
+
+function opladningScroll() {
+    document.getElementById("opladning-id").scrollIntoView();
+}
+
+function priserScroll() {
+    document.getElementById("priser-id").scrollIntoView();
+}
+
+function batteriScroll() {
+    document.getElementById("batteri-id").scrollIntoView();
+}
+
+function loveogreglerScroll() {
+    document.getElementById("love-id").scrollIntoView();
+}
